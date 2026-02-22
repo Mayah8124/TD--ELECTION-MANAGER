866bd93b3c8557d8c26730e90e85217d21832af5
mayahNeko <mayahibela@gmail.com>
2026-02-22 11:20:54 +0300

    feat: complete Q1 add countAllVotes() method

--- src/main/java/org/ceni/repository/DataRetriever.java
+++ src/main/java/org/ceni/repository/DataRetriever.java
@@ -1,4 +1,28 @@
 package org.ceni.repository;
 
+import org.ceni.*;
+import org.ceni.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DataRetriever {
+    DBConnection dbConnection = new DBConnection();
+
+    public long countAllVotes() {
+        String sql = "select count(id) as total_votes from vote";
+
+        try (Connection conn = dbConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            if (rs.next()) {
+                return rs.getLong("total_votes");
+            }
+            return 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
