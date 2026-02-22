8aa59692abbb2caffdf709a03be26ec2ad220c6d
mayahNeko <mayahibela@gmail.com>
2026-02-22 15:06:26 +0300

    fix: format main output

--- src/main/java/org/ceni/Main.java
+++ src/main/java/org/ceni/Main.java
@@ -22,10 +22,22 @@ public class Main {
 //        System.out.println(results);
 
         List<CandidateVoteCount> results = dataRetriever.countValidVotesByCandidate();
-        for (CandidateVoteCount candidateVoteCount : results) {
-            System.out.println("Candidat: " + candidateVoteCount.getCandidateName().getName()
-                    + " | valid vote: " + candidateVoteCount.getValidVoteCount().getCount());
+//        for (CandidateVoteCount candidateVoteCount : results) {
+//            System.out.println("Candidat: " + candidateVoteCount.getCandidateName().getName()
+//                    + " | valid vote: " + candidateVoteCount.getValidVoteCount().getCount());
+//        }
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < results.size(); i++) {
+            CandidateVoteCount candidateVoteCount = results.get(i);
+            sb.append(candidateVoteCount.getCandidateName().getName())
+                    .append(" = ")
+                    .append(candidateVoteCount.getValidVoteCount().getCount());
+            if (i != results.size() - 1) {
+                sb.append(", ");
+            }
         }
+        sb.append("]");
+        System.out.println(sb);
 
     }
 }
--- src/main/java/org/ceni/repository/DataRetriever.java
+++ src/main/java/org/ceni/repository/DataRetriever.java
@@ -102,4 +102,6 @@ public class DataRetriever {
         }
         return candidateVoteCounts;
     }
+
+
 }
