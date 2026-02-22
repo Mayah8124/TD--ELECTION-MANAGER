41136cf45236b749f3a4fde7f0037da8e128859d
mayahNeko <mayahibela@gmail.com>
2026-02-22 16:39:07 +0300

    feat: format console output for all tests

--- src/main/java/org/ceni/Main.java
+++ src/main/java/org/ceni/Main.java
@@ -14,40 +14,47 @@ public class Main {
     public static void main(String[] args) {
         DataRetriever dataRetriever = new DataRetriever();
 
-          long totalVotes = dataRetriever.countAllVotes();
-//        System.out.println("Total votes: " + totalVotes);
+        System.out.println("========================================");
 
-//        List<VoteTypeCount> results = dataRetriever.countVotesByType();
-//        for (VoteTypeCount v : results) {
-//            System.out.println(v.getVoteType() + " | " + v.getCount());
-//        }
-//        System.out.println(results);
+        long totalVotes = dataRetriever.countAllVotes();
+        System.out.println("Total votes: " + totalVotes);
+
+        System.out.println("========================================");
+
+        List<VoteTypeCount> result = dataRetriever.countVotesByType();
+        System.out.println(result);
+
+        System.out.println("========================================");
 
         List<CandidateVoteCount> results = dataRetriever.countValidVotesByCandidate();
-//        for (CandidateVoteCount candidateVoteCount : results) {
-//            System.out.println("Candidat: " + candidateVoteCount.getCandidateName().getName()
-//                    + " | valid vote: " + candidateVoteCount.getValidVoteCount().getCount());
-//        }
-//        StringBuilder sb = new StringBuilder("[");
-//        for (int i = 0; i < results.size(); i++) {
-//            CandidateVoteCount candidateVoteCount = results.get(i);
-//            sb.append(candidateVoteCount.getCandidateName().getName())
-//                    .append(" = ")
-//                    .append(candidateVoteCount.getValidVoteCount().getCount());
-//            if (i != results.size() - 1) {
-//                sb.append(", ");
-//            }
-//        }
-//        sb.append("]");
-//        System.out.println(sb);
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < results.size(); i++) {
+            CandidateVoteCount candidateVoteCount = results.get(i);
+            sb.append(candidateVoteCount.getCandidateName().getName())
+                    .append(" = ")
+                    .append(candidateVoteCount.getValidVoteCount().getCount());
+            if (i != results.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        System.out.println(sb);
+
+        System.out.println("========================================");
 
         VoteSummary voteSummary = dataRetriever.computeVoteSummary();
-//        System.out.println(voteSummary);
+        System.out.println(voteSummary);
+
+        System.out.println("========================================");
 
         Double computeTurnoutRate = dataRetriever.computeTurnoutRate();
-//        System.out.println("Taux de participation = " + computeTurnoutRate + "%");
+        System.out.println("Taux de participation = " + computeTurnoutRate + "%");
+
+        System.out.println("========================================");
 
         ElectionResult electionResult = dataRetriever.findWinner();
-        System.out.println( electionResult);
+        System.out.println(electionResult);
+
+        System.out.println("========================================");
     }
 }
